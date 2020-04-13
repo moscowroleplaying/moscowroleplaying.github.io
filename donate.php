f77c5f9e7610e0a7ecc0f2098c776f833bd1f647
Kirill114 <moscowrp@inbox.ru>
2020-04-13 13:32:35 +0300

    Commit Smitler

---
+++ donate.php
@@ -0,0 +1,129 @@
+﻿
+<title>Россия Дрифт</title>
+<link rel="shortcut icon" href="/favicon.ico">
+<meta name="keywords" content="gta, san andreas по интернету, gta, samp, role play, fresh, multiplayer, rp">
+<meta name="description" content="Начать играть в GTA San Andreas по сети">
+<meta content="/img/vklogo.png">
+<link rel="shortcut icon" href="ico.png">
+<link rel="stylesheet" href="css/style.css">
+
+<script type="text/javascript" src="/js/jquery.js"></script></head>
+<body id="home">
+  <header>
+    <div class="box top_menu_variant2">
+		<a href="/" class="logo"></a>
+      <ul class="topmenu">
+        <li><a href="index.php">Главная</a></li>
+        <li><a href="https://vk.com/id86774603">форум</a></li>
+		<li><a href="https://vk.com/rd.rdmpru">Новости</a></li>
+        <li><a href="donate.php" class="active">Магазин</a></li>
+       </ul>
+	   <a class="mons" href="#ModalOpen" title="">Мониторинг</a>
+    </div>
+  </header>
+</body>
+   <section class="content container">
+<center><h1>Пополнение счёта</h1>
+<form method="post" class="auth">	<center>
+		<table style="  width: 400px; background: #FFF;border-radius: 8px 8px 8px 8px/8px 8px 8px 8px;">
+			<tr>
+				<th>
+					<center>
+						<div class="fresh-input first">
+							<span class="pre icon-login"></span>
+							<h1>Ваш никнейм:</h1>
+							<input name="account" id="DonateForm_username" type="text" />					
+						</div>
+						<div class="fresh_error" id="DonateForm_username_em_" style="display:none"></div>					</center>
+				</th>
+			</tr>
+			<tr>
+				<th>
+					<center>
+						<div id="fresh-selector">
+						<h1>выберите сервер</h1>
+							<select id="fresh-selector" name="server">
+							<option selected disabled></option>
+								<option value="1">Россия Дрифт | Yellow</option>
+								<option value="2">Россия Дрифт | Green</option>
+								<option value="3"><img src="img/red.png">Россия Дрифт | Red</option>
+							</select>			
+							<div class="arrow"></div>
+						</div>						
+					</center>
+					<br>
+					<div class="fresh_error" id="DonateForm_system_em_" style="display:none"></div>				</th>
+			</tr>
+			<tr>
+				<th>
+					<center>
+						<div class="fresh-input">
+							<span class="pre icon-coin"></span>
+							<h1>Введите сумму</h1>
+							<input name="sum" id="DonateForm_sum" type="text" />					
+						</div>
+						<br>
+						<div class="fresh_error" id="DonateForm_sum_em_" style="display:none"></div>					</center>
+				</th>
+			</tr>			
+			<tr>
+				<b>
+					<th>
+						<input type="submit" name="BuyIt" class="submit" value="ОПЛАТИТЬ">
+					</th>
+				</b>
+			</tr>
+		</table>
+	</center>
+
+</form>
+</center>
+	</section>
+	<div id="ModalOpen" class="Window">
+ <div>
+ <div class="popup_statistic">
+    <section class="statistic v_align">
+        <div class="statistic_content">
+          <span class="menu_close"></span>
+          <div class="statistic_table">
+		  <h1>Мониторинги <a href="#close" title="Закрыть" class="close">x</a></h1>
+            <div class="headborder"></div>
+				<div class="statistic_table_block">
+				  <div class="statistic_table_content">
+					<div class="servers_row animated zoomInRight">
+				<table class="monitoring" cellspacing="0">
+								<tbody><tr>
+									<th>Игровой сервер</th>
+									<th>Игроков онлайн</th>
+									<th class="mob_hide">IP адрес</th>
+								</tr>
+								
+								
+						<tr>
+							<td class="red"><img src="img/red.png"/>Red</td>
+							<td>236/500</td>
+							<td><a class="ip" href="samp://IP:">83.222.105.54:8358</a></td>
+						</tr>
+						<tr>
+							<td class="yellow"><img src="img/yellow.png"/>Yellow</td>
+							<td>270/500</td>
+							<td><a class="ip" href="samp://IP:">83.222.105.54:8358</a></td>
+						</tr>
+						<tr>
+							<td class="green"><img src="img/green.png"/>Green</td>
+							<td>270/501</td>
+							<td><a class="ip" href="samp://IP:">83.222.105.54:8358</a></td>
+						</tr>
+								
+							</tbody>
+				</table>
+					</div>
+				  </div>
+				</div>
+           </div>
+        </div>
+    </div>
+ </div>
+</div>
+</div>
+</body></html>
---
+++ js/vknews.js
@@ -0,0 +1,59 @@
+﻿$(document).ready(function(){
+		function GetDate($date){
+		var t = new Date( $date*1000 );
+		var m = [t.getDate(), months[t.getMonth()],t.getYear()+1900];
+		return m;
+		}
+		var months = ['ЯНВАРЯ','ФЕВАЛЯ','МАРТА','АПРЕЛЯ','МАЯ','ИЮНЯ','ИЮЛЯ','АВГУСТА','СЕНТЯБРЯ','ОКТЯБРЯ','НОЯБРЯ','ДЕКАБРЯ'];
+		
+		
+		function GetPostWall(counts, $offset) {
+			var message = [];
+			var pinned = false;
+			var i = 1;
+			var req = "https://api.vk.com/method/wall.get?domain=fresh_rp&offset=" + $offset + "&count=" + (counts+1); // GET запрос по протоколу HTTPS;
+			$.ajax({
+				url: req,
+				type: "GET",
+				dataType: "jsonp",
+				success: function (msg) {
+					items = msg.response;
+
+					
+					
+					while (i <= counts) {
+							
+							if (items[i] != undefined){
+								if (items[i].text.length > 10){
+								message += '<div class="news" id="news-'+i+'"><img src="" class="news-image"><a href=https://vk.com/fresh_rp?w=wall'+ items[i].to_id +'_'+ items[i].id +'>Читать</a><div class="news-text">'+items[i].text +'</div><div class="like"><div class="like-btn"></div>Нравится <b>'+ items[i].likes.count +'</b></div>';
+								$("#news").append(message).show('slow');
+								message = "";
+								};
+							}
+							i++;
+
+						
+						
+					};
+					
+					
+					
+
+				}
+				
+			});
+		}
+		GetPostWall(3, 1);
+		$posts = 3;
+		$offsets = 3;
+	
+		$(window).scroll(function(){ 
+		if ($(document).height() - $(window).height() <= $(window).scrollTop() + 2500) { 
+			if($offsets < 3){
+				GetPostWall($posts, $offsets);
+				$offsets = $offsets + $posts;
+			}
+		} 
+		});
+	});
+	
